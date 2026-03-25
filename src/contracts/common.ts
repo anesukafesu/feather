8d6c1ff0ccbe63876517cb4d28959a9086aa5747
Anesu Kafesu <a.kafesu@alustudent.com>
2026-03-25 13:42:43 +0200

    Added dataset value type and modified dataset data type.

--- src/contracts/common.ts
+++ src/contracts/common.ts
@@ -2,7 +2,8 @@ export type DefaultValueStrategy =
   | { type: "auto_increment"; next_value: number }
   | { type: "use_random_uuid" };
 
-export type DatasetDataType = "text" | "number" | "boolean" | null;
+export type DatasetDataType = "text" | "number" | "boolean" | "null";
+export type DatasetValueType = string | number | boolean | null;
 
 export interface Column {
   name: string;
